19e24812056e2dc50ef137a26c95e5214e2a7726
Nghia Nguyen <nghianguyen3004@gmail.com>
2026-06-01 09:33:58 +0700

    Initial commit w/ server and db set up

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ message: "Server is running!" });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
